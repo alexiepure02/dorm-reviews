1cc08c2716aa3fff1710dae93ebdbb246493719b
Alex Iepure <alex.iepure02@gmail.com>
2023-02-03 10:59:18 +0200

    initial project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  // const postUser = async () => {
+  //   await fetch("http://localhost:3000/api/hello", {
+  //     method: "POST",
+  //   });
+  // };
+
+  // postUser();
+
+  return <h1>Hello.</h1>;
+}
