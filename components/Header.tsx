55093eb00191be6076c6f22c2e964a208a600c2c
Alex Iepure <alex.iepure02@gmail.com>
2023-04-20 17:56:22 +0300

    add sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,81 +5,85 @@ import { BiBed, BiBuildings, BiMapAlt, BiPencil, BiUser } from "react-icons/bi";
 import { MdOutlineBed, MdOutlineBedroomParent } from "react-icons/md";
 
 import Button from "./Button";
+import Sidebar from "./Sidebar";
 
 export default () => {
   const { data: session } = useSession();
   const pathname = usePathname();
 
   return (
-    <header className="sticky top-0 bg-white flex justify-between px-8 py-3 m-0 z-50 rounded-b-md shadow-md">
-      <Link href={"/"} className="flex gap-4 items-center cursor-pointer">
-        <img src="/logo.svg" className="w-14 h-16" />
-        <h1 className="font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-linear-1 to-linear-2">
-          Căminul Tău
-        </h1>
-      </Link>
-      <div className="hidden md:flex gap-8 items-center">
-        <Link
-          href={"/reviews?user=" + session?.user?.name}
-          className={`flex gap-2 font-semibold hover:text-primary-100 ${
-            pathname === "/reviews" && "text-primary-100"
-          }`}
-        >
-          <BiPencil
-            className={`w-6 h-6 text-gray-3 ${
+    <>
+      <Sidebar />
+      <header className="sticky top-0 bg-white flex justify-between px-8 py-3 m-0 z-50 rounded-b-xl shadow-md">
+        <Link href={"/"} className="flex gap-4 items-center cursor-pointer">
+          <img src="/logo.svg" className="w-14 h-16" />
+          <h1 className="hidden md:block font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-linear-1 to-linear-2">
+            Căminul Tău
+          </h1>
+        </Link>
+        <div className="hidden md:flex gap-8 items-center">
+          <Link
+            href={"/reviews?user=" + session?.user?.name}
+            className={`flex gap-2 font-semibold hover:text-primary-100 ${
               pathname === "/reviews" && "text-primary-100"
             }`}
-          />
-          Recenzii
-        </Link>
-        <Link
-          href={"/locations"}
-          className={`flex gap-2 font-semibold hover:text-primary-100 ${
-            pathname === "/locations" && "text-primary-100"
-          }`}
-        >
-          <BiMapAlt
-            className={`w-6 h-6 text-gray-3 ${
-              pathname === "/locations" && "text-primary-100"
-            }`}
-          />
-          Hartă
-        </Link>
-        <Link
-          href={"/universities"}
-          className={`flex gap-2 font-semibold hover:text-primary-100 ${
-            pathname === "/universities" && "text-primary-100"
-          }`}
-        >
-          <BiBuildings
-            className={`w-6 h-6 text-gray-3 ${
-              pathname === "/universities" && "text-primary-100"
-            }`}
-          />
-          Universități
-        </Link>
-
-        {session ? (
-          <Link
-            href={"/account"}
-            className={`flex gap-2 font-semibold hover:text-primary-100 ${
-              pathname === "/account" && "text-primary-100"
-            }`}
           >
-            <BiUser
+            <BiPencil
               className={`w-6 h-6 text-gray-3 ${
-                pathname === "/account" && "text-primary-100"
+                pathname === "/reviews" && "text-primary-100"
               }`}
             />
-            {session.user?.name}
+            Recenzii
           </Link>
-        ) : (
-          <Button className="w-[200px]">
-            <Link href={"/login"}>Autentificare</Link>
-          </Button>
-        )}
-      </div>
-    </header>
+          <Link
+            href={"/locations"}
+            className={`flex gap-2 font-semibold hover:text-primary-100 ${
+              pathname === "/locations" && "text-primary-100"
+            }`}
+          >
+            <BiMapAlt
+              className={`w-6 h-6 text-gray-3 ${
+                pathname === "/locations" && "text-primary-100"
+              }`}
+            />
+            Hartă
+          </Link>
+          <Link
+            href={"/universities"}
+            className={`flex gap-2 font-semibold hover:text-primary-100 ${
+              pathname === "/universities" && "text-primary-100"
+            }`}
+          >
+            <BiBuildings
+              className={`w-6 h-6 text-gray-3 ${
+                pathname === "/universities" && "text-primary-100"
+              }`}
+            />
+            Universități
+          </Link>
+
+          {session ? (
+            <Link
+              href={"/account"}
+              className={`flex gap-2 font-semibold hover:text-primary-100 ${
+                pathname === "/account" && "text-primary-100"
+              }`}
+            >
+              <BiUser
+                className={`w-6 h-6 text-gray-3 ${
+                  pathname === "/account" && "text-primary-100"
+                }`}
+              />
+              {session.user?.name}
+            </Link>
+          ) : (
+            <Button className="w-[200px]">
+              <Link href={"/login"}>Autentificare</Link>
+            </Button>
+          )}
+        </div>
+      </header>
+    </>
   );
   // if (session) {
   //   console.log(session);
---
+++ components/Sidebar.tsx
@@ -0,0 +1,94 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { slide as Menu } from "react-burger-menu"; // could also use slide or bubble
+import { BiBuildings, BiMapAlt, BiPencil, BiUser } from "react-icons/bi";
+import Button from "./Button";
+
+var styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    right: "32px",
+    top: "29px",
+  },
+  bmBurgerBars: {
+    background: "#EF9045",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "32px",
+    width: "32px",
+    marginRight: "29px",
+    marginTop: "22px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    height: "100%",
+  },
+  bmMenu: {
+    background: "#FDF4EC",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmMorphShape: {
+    fill: "#373a47",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+    display: "flex",
+    flexDirection: "column",
+    gap: "16px",
+  },
+  bmItem: {
+    display: "flex",
+    gap: "8px",
+    color: "#484848",
+    fontWeight: "600",
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)",
+  },
+};
+
+export default () => {
+  const { data: session } = useSession();
+  const pathname = usePathname();
+
+  return (
+    <div className="md:hidden">
+      <Menu styles={styles} right>
+        <Link href={"/reviews?user=" + session?.user?.name}>
+          <BiPencil className="w-6 h-6 text-gray-3" />
+          Recenzii
+        </Link>
+        <Link href={"/locations"}>
+          <BiMapAlt className="w-6 h-6 text-gray-3" />
+          Hartă
+        </Link>
+        <Link href={"/universities"}>
+          <BiBuildings className="w-6 h-6 text-gray-3" />
+          Universități
+        </Link>
+
+        {/* {session ? (
+          <Link href={"/account"}>
+            <BiUser className="w-6 h-6 text-gray-3" />
+            {session.user?.name}
+          </Link>
+        ) : ( */}
+          <Button className="w-[200px] mt-6">
+            <Link href={"/login"}>Autentificare</Link>
+          </Button>
+        {/* )} */}
+      </Menu>
+    </div>
+  );
+};
