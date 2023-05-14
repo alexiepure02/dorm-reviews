8d9c452981def0de1749a2cba16b0e9f470e41bd
Alex Iepure <alex.iepure02@gmail.com>
2023-05-14 15:41:27 +0300

    add layout and change metadata to account page

---
+++ app/(main)/account/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Cont - Căminul Tău",
+};
+
+export default function PasswordResetLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
--- app/(main)/account/page.tsx
+++ app/(main)/account/page.tsx
@@ -1,9 +1,27 @@
-import AccountSection from "./AccountSection";
+"use client";
 
-export const metadata = {
-  title: "Cont - Căminul Tău",
-};
+import Button from "@/components/Button";
+import { signOut, useSession } from "next-auth/react";
 
 export default async function Page() {
-  return <AccountSection />;
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex flex-col gap-6 items-center justify-center h-52">
+      {session !== null ? (
+        session !== undefined ? (
+          <>
+            <h1>You're logged in as: {session.user?.name}</h1>
+            <Button className="w-52" onClick={() => signOut()}>
+              Sign out
+            </Button>
+          </>
+        ) : (
+          <h1>Loading...</h1>
+        )
+      ) : (
+        <h1>You're not logged in.</h1>
+      )}
+    </div>
+  );
 }
